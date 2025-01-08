7c8fd6a442b8a63bd9ff6b975bb8c6e5c4ef9fd0
vm1828 <vm18281828@gmail.com>
2025-01-08 11:16:08 +0200

    Update Autokey configuration

--- scripts/autokey/data/Sample Scripts/0_CTRL_SHIFT_V.py
+++ scripts/autokey/data/Sample Scripts/0_CTRL_SHIFT_V.py
@@ -1,3 +1,8 @@
+import re
+
 text = clipboard.get_selection()
 text = " ".join(text.split())  # remove extra spaces and new lines
-keyboard.send_keys(text)
+
+pattern = r'\b\d{1,2}:\d{2}\b'  # "4:54", "12:34", etc.
+text = re.sub(pattern, '', text).strip()
+keyboard.send_keys(text+'\n')
