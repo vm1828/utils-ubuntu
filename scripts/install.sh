2d3556b65c33756fdc576832d541bc873d2b7aff
vm18281828 <vm18281828@gmail.com>
2025-05-09 22:18:58 +0300

    Add docker to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -42,6 +42,25 @@ install_flameshot() {
     sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm3/custom.conf
 }
 
+install_docker() {
+    # Add Docker's official GPG key:
+    sudo apt-get install ca-certificates curl -y
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+    # Install
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo usermod -aG docker $USER
+    newgrp docker
+}
+
 # List of apps to install (app_name, package_name, command)
 apps=(
 
@@ -57,6 +76,7 @@ apps=(
     "Peek" "peek" "sudo apt install peek -y"
     "Flameshot" "flameshot" "install_flameshot"
     "VSCode IDE" "code" "install_vscode"
+    "Docker" "docker" "install_docker"
 
     # snap apps
     "Postman" "postman" "sudo snap install postman"
